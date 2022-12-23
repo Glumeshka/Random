b423890434a0e8136fd1a9b32a57cc4cad2fb68e
Glumeshka <rzl1985@gmail.com>
2022-12-24 00:03:39 +0300

    first commit

---
+++ init.js
@@ -0,0 +1,33 @@
+window.onload = function() { // запуск при первом открывании в окне
+    getInit();
+}
+
+let mon, initPerson; // обявляем глобальные переменные
+
+function getInit() { // функция для запуска скрипта
+    mon = Math.floor(Math.random() * 3); //переменная необходимая для получения генерации месяца и дня 
+    initPerson = personGenerator.getPerson();
+    document.querySelector('#surnameOutput').innerText = initPerson.surname;
+    document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
+    document.querySelector('#patronymicOutput').innerText = initPerson.patronymic;
+    document.querySelector('#genderOutput').innerText = initPerson.gender;
+    document.querySelector('#birthDayOutput').innerText = initPerson.day;
+    document.querySelector('#birthMonthOutput').innerText = initPerson.month;
+    document.querySelector('#birthYearOutput').innerText = initPerson.year;
+    document.querySelector('#professionOutput').innerText = initPerson.profession;
+}
+
+document.querySelector('#generation').addEventListener('click', function() { // вешаем обработчик на кнопку "генерация"
+    getInit();
+});
+
+document.querySelector('#clear').addEventListener('click', function() { // вешаем обработчик на кнопку "очистка"
+    document.querySelector('#surnameOutput').innerText = 'Пусто';
+    document.querySelector('#firstNameOutput').innerText = 'Пусто';
+    document.querySelector('#patronymicOutput').innerText = 'Пусто';
+    document.querySelector('#genderOutput').innerText = 'Пусто ';
+    document.querySelector('#birthDayOutput').innerText = 'Пусто ';
+    document.querySelector('#birthMonthOutput').innerText = '';
+    document.querySelector('#birthYearOutput').innerText = '';
+    document.querySelector('#professionOutput').innerText = 'Пусто';
+});
---
+++ persongenerator.js
@@ -0,0 +1,223 @@
+const personGenerator = { // при count = 17 при методе Math.floor id_17 не достижим...
+    surnameJson: `{  
+        "count": 18, 
+        "list": {
+            "id_1": "Иванов",
+            "id_2": "Смирнов",
+            "id_3": "Кузнецов",
+            "id_4": "Васильев",
+            "id_5": "Петров",
+            "id_6": "Федулов",
+            "id_7": "Новиков",
+            "id_8": "Федоров",
+            "id_9": "Кравцев",
+            "id_10": "Симонов",
+            "id_11": "Семёнов",
+            "id_12": "Славин",
+            "id_13": "Степанов",
+            "id_14": "Павлов",
+            "id_15": "Александров",
+            "id_16": "Морозов",
+            "id_17": "Сидоров"
+        }
+    }`,
+    firstNameMaleJson: `{
+        "count": 18,
+        "list": {     
+            "id_1": "Александр",
+            "id_2": "Максим",
+            "id_3": "Иван",
+            "id_4": "Артем",
+            "id_5": "Дмитрий",
+            "id_6": "Никита",
+            "id_7": "Михаил",
+            "id_8": "Даниил",
+            "id_9": "Егор",
+            "id_10": "Владимир",
+            "id_11": "Антон",
+            "id_12": "Чеслав",
+            "id_13": "Вадим",
+            "id_14": "Пётр",
+            "id_15": "Денис",
+            "id_16": "Афанасий",
+            "id_17": "Аркадий"
+        }
+    }`,
+    firstNameFemaleJson: `{
+        "count": 18,
+        "list": {     
+            "id_1": "Мария",
+            "id_2": "Анна",
+            "id_3": "Ирина",
+            "id_4": "Елена",
+            "id_5": "Наталья",
+            "id_6": "Ольга",
+            "id_7": "Светлана",
+            "id_8": "Галина",
+            "id_9": "Людмила",
+            "id_10": "Татьяна",
+            "id_11": "Юлия",
+            "id_12": "Берта",
+            "id_13": "Беатриса",
+            "id_14": "Вера",
+            "id_15": "Вероника",
+            "id_16": "Василиса",
+            "id_17": "Глафира"
+        }
+    }`,
+    patronymicJson: `{
+        "count": 18,
+        "list": {
+            "id_1": "Владимиров",
+            "id_2": "Алексеев",
+            "id_3": "Сергеев",
+            "id_4": "Петров",
+            "id_5": "Андреев",
+            "id_6": "Михайлов",
+            "id_7": "Иванов",
+            "id_8": "Александров",
+            "id_9": "Анатольев",
+            "id_10": "Вадимов",
+            "id_11": "Аркадьев",
+            "id_12": "Афанасьев",
+            "id_13": "Денисов",
+            "id_14": "Чеславов",
+            "id_15": "Данилов",
+            "id_16": "Антонов",
+            "id_17": "Артемов"
+        }
+    }`,
+    professionMaleJson: `{
+        "count": 18,
+        "list": {
+            "id_1": "Сварщик",
+            "id_2": "Тракторист",
+            "id_3": "Пианист",
+            "id_4": "Флейтист",
+            "id_5": "Кузнец",
+            "id_6": "Слесарь",
+            "id_7": "Адвокат",
+            "id_8": "Таксист",
+            "id_9": "Строитель",
+            "id_10": "Дворецкий",
+            "id_11": "Телохранитель",
+            "id_12": "Поп",
+            "id_13": "Лесник",
+            "id_14": "Актер",
+            "id_15": "Стример",
+            "id_16": "Пожарный",
+            "id_17": "Реставратор"
+        }
+    }`,
+    professionFemaleJson: `{
+        "count": 18,
+        "list": {
+            "id_1": "Воспитатель",
+            "id_2": "Мастер маникюра",
+            "id_3": "Посудомойка",
+            "id_4": "Проводница",            
+            "id_5": "Парикмахер",
+            "id_6": "Горничная",
+            "id_7": "Связистка",
+            "id_8": "Актриса",
+            "id_9": "Секретарь",
+            "id_10": "Консультант",
+            "id_11": "Библиотекарь",
+            "id_12": "Йога-инструктор",
+            "id_13": "Уборщица",
+            "id_14": "Стюардесса",
+            "id_15": "Крановщица",
+            "id_16": "Танцовщица",
+            "id_17": "Директриса"
+        }
+    }`,
+
+    GENDER_MALE: 'Мужской, ', // переписано для наглядности в форме
+    GENDER_FEMALE: 'Женский, ',
+
+    randomGender: function() { // метод для генерации М/Ж
+        return Math.floor(Math.random()*2) == 1 ? this.GENDER_MALE : this.GENDER_FEMALE;
+    },
+
+    randomIntNumber: (min = 0, max = 1) => Math.floor(Math.random() * (max - min + 1) + min), // исходный метод
+
+    randomValue: function (json) {
+        const obj = JSON.parse(json);
+        const prop = `id_${this.randomIntNumber(obj.count, 1)}`;
+        return obj.list[prop];
+    },
+
+    randomFirstName: function() { // метод генерации Имени
+        if (this.person.gender == 'Мужской, ') {
+            return this.randomValue(this.firstNameMaleJson);
+        } else {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
+    },
+
+    randomPatronymic: function() { // метод генерации отчества
+        if (this.person.gender == 'Мужской, ') {
+            return this.randomValue(this.patronymicJson) + "ич";
+        } else {
+            return this.randomValue(this.patronymicJson) + "на";
+        }
+    },
+
+    randomSurname: function() { // метод генерации Фамилии
+        if (this.person.gender == 'Мужской, ') {
+            return this.randomValue(this.surnameJson);
+        } else {
+            return this.randomValue(this.surnameJson) + "а";
+        }
+    },
+
+    randomРrofession: function() { // метод генерации Проффесии
+        if (this.person.gender == 'Мужской, ') {
+            return this.randomValue(this.professionMaleJson);
+        } else {
+            return this.randomValue(this.professionFemaleJson);
+        }
+    },
+
+    randomMonth31: function randomMonth() { // методика для генерации месяцев и сдней
+        let months = [`января`, `марта`, `мая`,	`июля`,	`августа`, `октября`, `декабря`];
+        let month = months[Math.floor(Math.random() * 7)];
+        return month;
+    },
+    
+    randomMonth30: function randomMonth() {
+        let months = [`апреля`, `июня`, `сентября`, `ноября`];
+        let month = months[Math.floor(Math.random() * 4)];
+        return month;
+    },
+
+    randomMonthFeb28: function randomMonth() {
+		let month = `февраля`
+		return month;
+	},
+    
+    rendomYear: function() { // метод генерации года рождения
+        return this.randomIntNumber(1960, 2000) + " г.";
+    },
+    
+    getPerson: function() { // метод получения данных от генерации для передачи их в функцию
+        this.person = {};
+        this.person.gender = this.randomGender();
+        this.person.surname = this.randomSurname();
+        this.person.firstName = this.randomFirstName();
+        this.person.patronymic = this.randomPatronymic();
+        if (mon === 0) { // переменная в файле init.js
+            this.person.month = this.randomMonth31();
+            this.person.day = this.randomIntNumber(1, 31);
+        } else if (mon === 1) {
+            this.person.month = this.randomMonth30();
+            this.person.day = this.randomIntNumber(1, 30);
+        } else if (mon === 2) {
+            this.person.month = this.randomMonthFeb28();
+            this.person.day = this.randomIntNumber(1, 28);
+        }
+        this.person.year = this.rendomYear();
+        this.person.profession = this.randomРrofession();
+        return this.person;
+    }
+};
